1ba966851464ca01aaf597d96a35842da0e2d585
richik1997 <36633889+richik1997@users.noreply.github.com>
2018-04-09 20:27:36 +0530

    Add files via upload

---
+++ event_redirect.php
@@ -0,0 +1,12 @@
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>lo
+<script type="text/javascript">
+  $(window).on('load resize',function(){
+        if($(window).width() < 99){
+            window.location = "events.html"
+        }
+        else
+        {
+        	window.location = "event.html"
+        }
+    });
+</script>
